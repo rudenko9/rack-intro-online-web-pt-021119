d8e35caf6ec09b648b6f41b793ade41cb6080f0c
Nataliya R <n.rudenko@gmail.com>
2019-05-15 02:32:25 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -5,7 +5,11 @@ class Application
     resp.write "Hello, World"
     resp.finish
   end
-
+def last_response(env)
+  resp = Rack::Response.new 
+  resp.write "Hello, my name is Nataliya"
+  resp.finish
+end
 
 end
 
